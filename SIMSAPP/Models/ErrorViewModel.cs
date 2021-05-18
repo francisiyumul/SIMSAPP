9a2a588d8dcf1e1010677abf34354202d9ea36d8
FrancisY <FrancisY@PC004>
2021-05-18 17:13:30 +0400

    Add project files.

---
+++ SIMSAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SIMSAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        //TEST
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
